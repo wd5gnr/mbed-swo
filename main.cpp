7429892ab252571fc42f2d5918150ea86d01f2d3
Al Williams <al.williams@awce.com>
2022-05-22 14:02:09 -0500

    Added optional channel number to constructor

--- main.cpp
+++ main.cpp
@@ -4,123 +4,44 @@
  */
 
 #include "mbed.h"
-#include "USBSerial.h"
-
-#include "Encoder.h"
-
-#define TEST_LATENCY 0
+#include "SWO.h"
 
 
 
 // Blinking rate in milliseconds
-std::chrono::milliseconds rate=500ms;
+std::chrono::milliseconds rate=150ms;
 
 
 
 
 
 
-Thread encthread;    // thread for encoder
-Thread stdio_clear;  // thread to watch for USB terminal connection
 
-    // Note: init to false requires connect to enumerate
-    // If true, you must connect to the board to start
-USBSerial usbSerial(false);
-
-
-
-// If you want to use printf etc with USB Serial...
-// HOWEVER. USBSerial must be connected prior to elaboration or no go
-namespace mbed
-{
-    FileHandle *mbed_override_console(int fd)
-    {
-        return &usbSerial;
-    }
-
-}
-
-
-// This thread watches for a USB terminal connection and resets stdio to work with it
-void stdio_reset()
-{
-    int conn,last=0;
-    while (1)
-    {
-    if ((conn=usbSerial.connected()) && !last)  // note NOT conn==usbSerial....
-    {
-       clearerr(stdout);
-       clearerr(stderr);
-       clearerr(stdin);
-    }
-    last=conn;   // remember connection state
-    ThisThread::sleep_for(500ms);  // check again later
-    }
-}
 
 DigitalOut led(LED1);
 
+const char *color[]={ "#GRN#", "#RED#", "#ORG#" };
 
-#if TEST_LATENCY==0 
-Encoder encoder(PA_3,PA_4);  // our encoder
-
-// The encoder thread
-void do_enc(void)
-{
-    int evalue=0;
-    encoder.setAccumulate(true);  // let the class keep track of counts; note it can roll over, though
-    while (1)
-    {
-        evalue=encoder.read();
-        printf("Encoder: %d\r\n",evalue);
-         ThisThread::sleep_for(750ms);
+SWO_Channel debugport;
+SWO_Channel dbg2("X",1);
+SWO_Channel leddbg("LED",5);
+  int main() 
+  {
+      unsigned count=0;
+      debugport.printf("\r\nHello World from SWO\r\n");
+      debugport.printf("CPU SystemCoreClock is %d Hz\r\n", SystemCoreClock);
+  
+    while(1) {
+      led = !led;   // flip LED if output is true
+      
+        ThisThread::sleep_for(rate);  // sleepy time
+    
+      if (count % 10) debugport.putc('*'); else debugport.printf("%s%d\r\n",color[(count/10)%3],count); 
+      dbg2.printf("\r\nCount=%d",count);
+      leddbg.printf("\r\nState of LED=%d",led.read());
+      count++;
     }
-}
-#else
-
-InterruptIn testin(PA_3);
-
-void ledon(void)
-{
-    led=true;
-}
-
-void ledoff(void)
-{
-    led=false;
-}
-
-
-void do_enc(void)
-{
-    testin.rise(callback(ledon));
-    testin.fall(callback(ledoff));
-    while (1) ThisThread::sleep_for(1s);
-}
-
-#endif
-
-// Main thread
-int main()
-{
-    usbSerial.connect();  // connect USB port
-    // Initialise the digital pin LED1 as an output
+  }
     
 
-// If you redirect stdin/stdout etc to USBSerial, you have a problem
-// When there is no serial port open at run time. The stream gets an error and
-// then nothing will happen until you clear it.
-// So the trick is to occasionally clear errors on stdout/stdin/stderr
 
-    printf("Here we go!\r\n");  // Get started
-// start the threads
-    stdio_clear.start(stdio_reset);  
-    encthread.start(do_enc);
-// let us know we are running
-    while (true) {
-#if TEST_LATENCY==0
-        led = !led;   // flip LED if output is true
-#endif        
-        ThisThread::sleep_for(rate);  // sleepy time
-    }
-}
